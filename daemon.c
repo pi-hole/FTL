39abd05540185e7bd677a6103197ac1314e22629
DL6ER <dl6er@dl6er.de>
2017-04-14 18:04:42 +0200

    Fork twice to prevent the daemon from being a session leader - it cannot take control of a TTY any more - it is truely daemonized

--- daemon.c
+++ daemon.c
@@ -107,7 +107,7 @@ void go_daemon(void)
 	// PARENT PROCESS. Need to kill it.
 	if (process_id > 0)
 	{
-		printf("FTL started (new PID %i)!\n", process_id);
+		printf("FTL started!\n");
 		// return success in exit status
 		exit(EXIT_SUCCESS);
 	}
@@ -124,6 +124,33 @@ void go_daemon(void)
 		logg("setsid failed!\n");
 		exit(EXIT_FAILURE);
 	}
+
+	// Create grandchild process
+	// Fork a second child and exit immediately to prevent zombies.  This
+	// causes the second child process to be orphaned, making the init
+	// process responsible for its cleanup.  And, since the first child is
+	// a session leader without a controlling terminal, it's possible for
+	// it to acquire one by opening a terminal in the future (System V-
+	// based systems).  This second fork guarantees that the child is no
+	// longer a session leader, preventing the daemon from ever acquiring
+	// a controlling terminal.
+	process_id = fork();
+
+	// Indication of fork() failure
+	if (process_id < 0)
+	{
+		logg("fork failed!\n");
+		// Return failure in exit status
+		exit(EXIT_FAILURE);
+	}
+
+	// PARENT PROCESS. Need to kill it.
+	if (process_id > 0)
+	{
+		// return success in exit status
+		exit(EXIT_SUCCESS);
+	}
+
 	savepid();
 
 	// Change the current working directory
