6165dc651b2fc06b6d162cfc7461a342a89bd390
Mcat12 <newtoncat12@yahoo.com>
2018-09-09 14:26:19 -0400

    Fix exit message showing invalid program run time
    The exit timer was not started.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ int main (int argc, char* argv[])
 
 	// Try to open FTL log
 	open_FTL_log(true);
+	timer_start(EXIT_TIMER);
 	logg("########## FTL started! ##########");
 	log_FTL_version();
 	init_thread_lock();
