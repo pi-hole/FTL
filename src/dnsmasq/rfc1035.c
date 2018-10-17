598d19614db3d897d9b688dceb9c9af6b9442b91
Vladislav Grishenko <themiron@mail.ru>
2018-10-18 04:55:21 +0500

    Don't forward *.bind/*.server queries upstream
    Chaos .bind and .server (RFC4892) zones are local, therefore
    don't forward queries upstream to avoid mixing with supported
    locally and false replies with NO_ID enabled.
    
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/rfc1035.c
+++ src/dnsmasq/rfc1035.c
@@ -1277,7 +1277,7 @@ size_t answer_request(struct dns_header *header, char *limit, size_t qlen,
   int q, ans, anscount = 0, addncount = 0;
   int dryrun = 0;
   struct crec *crecp;
-  int nxdomain = 0, auth = 1, trunc = 0, sec_data = 1;
+  int nxdomain = 0, notimp = 0, auth = 1, trunc = 0, sec_data = 1;
   struct mx_srv_record *rec;
   size_t len;
 
@@ -1354,6 +1354,17 @@ size_t answer_request(struct dns_header *header, char *limit, size_t qlen,
 
 		    }
 		}
+	    }
+	}
+
+      if (qclass == C_CHAOS)
+	{
+	  /* don't forward *.bind and *.server chaos queries */
+	  if (hostname_issubdomain("bind", name) || hostname_issubdomain("server", name))
+	    {
+	      if (!ans)
+		notimp = 1, auth = 0;
+	      ans = 1;
 	    }
 	}
 
@@ -1944,6 +1955,8 @@ size_t answer_request(struct dns_header *header, char *limit, size_t qlen,
   
   if (nxdomain)
     SET_RCODE(header, NXDOMAIN);
+  else if (notimp)
+    SET_RCODE(header, NOTIMP);
   else
     SET_RCODE(header, NOERROR); /* no error */
   header->ancount = htons(anscount);
