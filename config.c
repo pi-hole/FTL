41e9a8a49ea8107083b3e353578f25c7d6cf640e
DL6ER <dl6er@dl6er.de>
2018-11-09 19:30:56 +0100

    Don't warn if privacy level is reduced. This generates too much output when using the web interface.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- config.c
+++ config.c
@@ -327,17 +327,11 @@ void get_privacy_level(FILE *fp)
 	{
 		// Check for change and validity of privacy level (set in FTL.h)
 		if(value >= PRIVACY_SHOW_ALL &&
-		   value <= PRIVACY_NOSTATS)
+		   value <= PRIVACY_NOSTATS &&
+		   value > config.privacylevel)
 		{
-			if(value > config.privacylevel)
-			{
-				logg("Notice: Increasing privacy level from %i to %i", config.privacylevel, value);
-				config.privacylevel = value;
-			}
-			else if(value < config.privacylevel)
-			{
-				logg("WARN: Restart pihole-FTL to use a privacy level lower than %i", config.privacylevel);
-			}
+			logg("Notice: Increasing privacy level from %i to %i", config.privacylevel, value);
+			config.privacylevel = value;
 		}
 	}
 
