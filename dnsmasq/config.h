1404ea167945fcbffa80198505352a799b0d4557
DL6ER <dl6er@dl6er.de>
2019-02-05 06:10:05 +0100

    Drop "root" to "root" in case started on a system without capabilities support to prevent file access issues and without any "user=..." and "group=..." settings in any dnsmasq config file. This effectively sets the default values for these options to "root". Note that this will not affect the usual Pi-hole operation (where FTL is started as user "pihole") as the user/group dropping is only done if dnsmasq detects that it was started as root (criterion is UID == 0).
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- dnsmasq/config.h
+++ dnsmasq/config.h
@@ -43,8 +43,8 @@
 #define HOSTSFILE "/etc/hosts"
 #define ETHERSFILE "/etc/ethers"
 #define DEFLEASE 3600 /* default lease time, 1 hour */
-#define CHUSER "nobody"
-#define CHGRP "dip"
+#define CHUSER "root"
+#define CHGRP "root"
 #define TFTP_MAX_CONNECTIONS 50 /* max simultaneous connections */
 #define LOG_MAX 5 /* log-queue length */
 #define RANDFILE "/dev/urandom"
