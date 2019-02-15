19db306bd966fa307a5d208e5b178fb41df6c165
DL6ER <dl6er@dl6er.de>
2019-02-15 20:27:34 +0100

    Remove compiler flags that are not supported by the Raspbian's GCC
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- dnsmasq_interface.c
+++ dnsmasq_interface.c
@@ -177,7 +177,6 @@ void FTL_new_query(unsigned int flags, char *name, struct all_addr *addr, char *
 	// Update overTime data structure with the new client
 	clients[clientID].overTime[timeidx]++;
 
-
 	// Try blocking regex if configured
 	validate_access("domains", domainID, false, __LINE__, __FUNCTION__, __FILE__);
 	if(domains[domainID].regexmatch == REGEX_UNKNOWN && blockingstatus != BLOCKING_DISABLED)
