83dc51aba1cfd93ac1274bb6ff6711f496ab889b
Mcat12 <newtoncat12@yahoo.com>
2019-02-15 20:32:45 -0800

    Add license header to overTime.c
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- overTime.c
+++ overTime.c
@@ -1,3 +1,13 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  FTL Engine
+*  MessagePack serialization
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
 #include "FTL.h"
 
 /**
