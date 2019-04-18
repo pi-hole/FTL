f790f15a2f700274433217017431e81a2afc1f8d
DL6ER <dl6er@dl6er.de>
2019-04-18 20:58:47 +0200

    Do not use backtrace_symbols() in backtrace generation when not compiling for glibc
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- signals.c
+++ signals.c
@@ -12,9 +12,9 @@
 #include <execinfo.h>
 
 volatile sig_atomic_t killed = 0;
-time_t FTLstarttime = 0;
+static time_t FTLstarttime = 0;
 
-static void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
+static void __attribute__((noreturn)) SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 {
 	logg("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	logg("---------------------------->  FTL crashed!  <----------------------------");
@@ -29,7 +29,7 @@ static void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 	log_FTL_version(true);
 
 	logg("Received signal: %s", strsignal(sig));
-	logg("     at address: %lu", (unsigned long) si->si_addr);
+	logg("     at address: %p", si->si_addr);
 	switch (si->si_code)
 	{
 		case SEGV_MAPERR: logg("     with code: SEGV_MAPERR (Address not mapped to object)"); break;
@@ -37,28 +37,37 @@ static void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 #if defined(SEGV_BNDERR)
 		case SEGV_BNDERR: logg("     with code: SEGV_BNDERR (Failed address bound checks)"); break;
 #endif
-		default: logg("     with code: Unknown (%i), ",si->si_code); break;
+		default: logg("     with code: Unknown (%i)", si->si_code); break;
 	}
 
 	// Try to obtain backtrace. This may not always be helpful, but it is better than nothing
 	void *buffer[255];
 	const int calls = backtrace(buffer, sizeof(buffer)/sizeof(void *));
+
+// Check GLIBC availability as MUSL does not support backtrace_symbols()
+#if defined(__GLIBC__)
 	char ** bcktrace = backtrace_symbols(buffer, calls);
 	if(bcktrace == NULL)
 	{
-		logg("Unable to obtain backtrace (%i)!",calls);
+		logg("Unable to obtain backtrace (%i)!", calls);
 	}
 	else
 	{
 		logg("Backtrace:");
-		int j;
-		for (j = 0; j < calls; j++)
+		for(int j = 0; j < calls; j++)
 		{
-			logg("B[%04i]: %s",j,bcktrace[j]);
+			logg("B[%04i]: %s", j, bcktrace[j]);
 		}
 	}
 	free(bcktrace);
-
+#else
+	logg("!!! INFO: pihole-FTL has not been compiled for glibc !!!");
+	logg("Backtrace:");
+	for(int j = 0; j < calls; j++)
+	{
+		logg("B[%04i]: %p", j, buffer[j]);
+	}
+#endif
 	logg("Thank you for helping us to improve our FTL engine!");
 
 	// Print message and abort
