cf8b3f0d22050acdf6c080f43c75a8cfcc5d4c37
DDoSolitary <DDoSolitary@gmail.com>
2019-04-28 08:09:22 +0800

    Use <poll.h> instead of <sys/poll.h>
    The former should be used according to POSIX, otherwise it causes
    bunches of warnings when compiling for musl-based distros like Alpine
    Linux.
    
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/dnsmasq.h
+++ src/dnsmasq/dnsmasq.h
@@ -95,7 +95,11 @@ typedef unsigned long long u64;
 #if defined(HAVE_SOLARIS_NETWORK)
 #  include <sys/sockio.h>
 #endif
-#include <sys/poll.h>
+#if defined(HAVE_POLL_H)
+#  include <poll.h>
+#else
+#  include <sys/poll.h>
+#endif
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/un.h>
