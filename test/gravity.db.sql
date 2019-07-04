62edeb2a5a6dca0a88846e0f52f112136f00dd02
DL6ER <dl6er@dl6er.de>
2019-07-04 22:19:56 +0200

    Add a blacklisted domain that is disabled through a group association.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- test/gravity.db.sql
+++ test/gravity.db.sql
@@ -141,10 +141,14 @@ CREATE TRIGGER tr_adlist_update AFTER UPDATE ON adlist
       UPDATE adlist SET date_modified = (cast(strftime('%s', 'now') as int)) WHERE address = NEW.address;
     END;
 
-INSERT INTO whitelist VALUES(1,'whitelisted.com',1,1559928803,1559928803,0,'Migrated from /etc/pihole/whitelist.txt');
-INSERT INTO blacklist VALUES(1,'blacklisted.com',1,1559928803,1559928803,0,'Migrated from /etc/pihole/blacklist.txt');
-INSERT INTO regex VALUES(1,'regex[0-9].com',1,1559928803,1559928803,0,'Migrated from /etc/pihole/regex.list');
-INSERT INTO adlists VALUES(1,'https://hosts-file.net/ad_servers.txt',1,1559928803,1559928803,0,'Migrated from /etc/pihole/adlists.list');
+INSERT INTO whitelist VALUES(1,'whitelisted.com',1,1559928803,1559928803,'Migrated from /etc/pihole/whitelist.txt');
+INSERT INTO blacklist VALUES(1,'blacklisted.com',1,1559928803,1559928803,'Migrated from /etc/pihole/blacklist.txt');
+INSERT INTO regex VALUES(1,'regex[0-9].com',1,1559928803,1559928803,'Migrated from /etc/pihole/regex.list');
+INSERT INTO adlist VALUES(1,'https://hosts-file.net/ad_servers.txt',1,1559928803,1559928803,0,'Migrated from /etc/pihole/adlists.list');
 INSERT INTO gravity VALUES('0427d7.se');
-INSERT INTO info VALUES('version','1');
+
+INSERT INTO "group" VALUES(1,0,'Test group','A disabled test group');
+INSERT INTO blacklist VALUES(2,'blacklisted-group-disabled.com',1,1559928803,1559928803,'Entry disabled by a group');
+INSERT INTO blacklist_by_group VALUES(2,1);
+
 COMMIT;
