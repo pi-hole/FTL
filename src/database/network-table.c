7fae126ac47eacdca7d8cc67b3319227c1a39dc9
DL6ER <dl6er@dl6er.de>
2020-04-02 04:19:41 +0000

    Only check/set client status when size of the array is not exceeded. Skip otherwise.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/database/network-table.c
+++ src/database/network-table.c
@@ -421,7 +421,7 @@ void parse_neighbor_cache(void)
 
 		// This client is known (by its IP address) to pihole-FTL if
 		// findClientID() returned a non-negative index
-		if(clientID >= 0)
+		if(clientID >= 0 && clientID < clients_array_size)
 		{
 			client_status[clientID] = CLIENT_ARP_COMPLETE;
 			client = getClient(clientID, true);
@@ -552,8 +552,9 @@ void parse_neighbor_cache(void)
 		}
 		// Skip if already handled above (first check against clients_array_size as we might have added
 		// more clients to FTL's memory herein (those known only from the database))
-		else if(clientID < clients_array_size && 
-		        client_status[clientID] != CLIENT_NOT_HANDLED)
+		else if((clientID < clients_array_size &&
+		         client_status[clientID] != CLIENT_NOT_HANDLED) ||
+			clientID >= clients_array_size)
 		{
 			if(config.debug & DEBUG_ARP)
 				logg("Network table: Client %s known through ARP/neigh cache",
