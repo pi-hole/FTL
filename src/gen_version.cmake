c2a163be89b46b1132f356fca48269b4a2baf2b7
DL6ER <dl6er@dl6er.de>
2020-05-26 20:38:36 +0200

    Add license headers to CMake files
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/gen_version.cmake
+++ src/gen_version.cmake
@@ -1,3 +1,13 @@
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2020 Pi-hole, LLC (https://pi-hole.net)
+# Network-wide ad blocking via your own hardware.
+#
+# FTL Engine
+# /src/gen_version.cmake
+#
+# This file is copyright under the latest version of the EUPL.
+# Please see LICENSE file for your rights under this license.
+
 if(NOT DEFINED GIT_BRANCH)
     execute_process(
             COMMAND           bash -c "git branch | sed -n 's/^\\* //p'"
