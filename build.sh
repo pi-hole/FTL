041f93f006376a00d787d1570583ffcf7ca6fc9e
DL6ER <dl6er@dl6er.de>
2020-05-27 19:02:25 +0200

    Improve build.sh script. Add "install" and "clean" targets. Also ensure successive builds are possible to speed up the entire process.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- build.sh
+++ build.sh
@@ -9,9 +9,27 @@
 # This file is copyright under the latest version of the EUPL.
 # Please see LICENSE file for your rights under this license.
 
-rm -rf cmake/ && \
-mkdir cmake && \
-cd cmake && \
-cmake .. && \
-cmake --build . -- -j $(nproc) && \
-cp pihole-FTL ../
+# Abort script if one command returns a non-zero value
+set -e
+
+# Prepare build environment
+if [[ "${1}" == "clean" ]]; then
+    rm -rf cmake/
+    exit 0
+fi
+
+# Configure build
+mkdir -p cmake
+cd cmake
+cmake ..
+
+# Build the sources
+cmake --build . -- -j $(nproc)
+
+# If we are asked to install, we do this here
+# Otherwise, we simply copy the binary one level up
+if [[ "${1}" == "install" ]]; then
+    sudo make install
+else
+    cp pihole-FTL ../
+fi
