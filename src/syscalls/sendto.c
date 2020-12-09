543ea8558725faa094f0587588dcd7f3db7d0100
DL6ER <dl6er@dl6er.de>
2020-12-09 20:03:55 +0100

    Add interrupt-safe sendto() routine
    Signed-off-by: DL6ER <dl6er@dl6er.de>

---
+++ src/syscalls/sendto.c
@@ -0,0 +1,38 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2020 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  FTL Engine
+*  Pi-hole syscall implementation for sendto
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
+#include "../FTL.h"
+//#include "syscalls.h" is implicitly done in FTL.h
+#include "../log.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+ssize_t FTLsendto(int sockfd, void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen, const char *file, const char *func, const int line)
+{
+    ssize_t ret = 0;
+	do
+	{
+		// Reset errno before trying to write
+		errno = 0;
+		ret = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+	}
+	// Try again if the last accept() call failed due to an interruption by an
+	// incoming signal
+	while(ret < 0 && errno == EINTR);
+
+	// Final error checking (may have faild for some other reason then an
+	// EINTR = interrupted system call)
+	if(ret < 0)
+		logg("WARN: Could not sendto() in %s() (%s:%i): %s",
+             func, file, line, strerror(errno));
+
+    return ret;
+}
--- src/syscalls/syscalls.h
+++ src/syscalls/syscalls.h
@@ -28,6 +28,7 @@ int FTLaccept(int sockfd, struct sockaddr *addr, socklen_t *addrlen, const char 
 ssize_t FTLrecv(int sockfd, void *buf, size_t len, int flags, const char *file, const char *func, const int line);
 ssize_t FTLrecvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen, const char *file, const char *func, const int line);
 int FTLselect(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout, const char *file, const char *func, const int line);
+ssize_t FTLsendto(int sockfd, void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen, const char *file, const char *func, const int line);
 
 // Interrupt-safe thread routines
 int FTLpthread_mutex_lock(pthread_mutex_t *__mutex, const char *file, const char *func, const int line);
