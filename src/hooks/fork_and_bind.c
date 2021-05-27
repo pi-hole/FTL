47ce7020e6c840ca658ca20a0a37f0a4e8040046
DL6ER <dl6er@dl6er.de>
2021-05-27 12:56:03 +0200

    Use specific log routines in hooks/fork_and_bind.c
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/hooks/fork_and_bind.c
+++ src/hooks/fork_and_bind.c
@@ -56,7 +56,7 @@ void FTL_fork_and_bind_sockets(struct passwd *ent_pw)
 	// Start database thread if database is used
 	if(pthread_create( &threads[DB], &attr, DB_thread, NULL ) != 0)
 	{
-		logg("Unable to open database thread. Exiting...");
+		log_crit("Unable to open database thread. Exiting...");
 		exit(EXIT_FAILURE);
 	}
 
@@ -64,7 +64,7 @@ void FTL_fork_and_bind_sockets(struct passwd *ent_pw)
 	// collection needs to be done
 	if(pthread_create( &threads[GC], &attr, GC_thread, NULL ) != 0)
 	{
-		logg("Unable to open GC thread. Exiting...");
+		log_crit("Unable to open GC thread. Exiting...");
 		exit(EXIT_FAILURE);
 	}
 
@@ -72,7 +72,7 @@ void FTL_fork_and_bind_sockets(struct passwd *ent_pw)
 	// needs to be resolved
 	if(pthread_create( &threads[DNSclient], &attr, DNSclient_thread, NULL ) != 0)
 	{
-		logg("Unable to open DNS client thread. Exiting...");
+		log_crit("Unable to open DNS client thread. Exiting...");
 		exit(EXIT_FAILURE);
 	}
 
@@ -84,19 +84,19 @@ void FTL_fork_and_bind_sockets(struct passwd *ent_pw)
 		// we're actually dropping root (user/group my be set to root)
 		if(ent_pw != NULL && ent_pw->pw_uid != 0)
 		{
-			logg("INFO: FTL is going to drop from root to user %s (UID %d)",
-			     ent_pw->pw_name, (int)ent_pw->pw_uid);
+			log_info("FTL is going to drop from root to user %s (UID %d)",
+			         ent_pw->pw_name, (int)ent_pw->pw_uid);
 			if(chown(FTLfiles.log, ent_pw->pw_uid, ent_pw->pw_gid) == -1)
-				logg("Setting ownership (%i:%i) of %s failed: %s (%i)",
+				log_warn("Setting ownership (%i:%i) of %s failed: %s (%i)",
 				ent_pw->pw_uid, ent_pw->pw_gid, FTLfiles.log, strerror(errno), errno);
 			if(chown(FTLfiles.FTL_db, ent_pw->pw_uid, ent_pw->pw_gid) == -1)
-				logg("Setting ownership (%i:%i) of %s failed: %s (%i)",
+				log_warn("Setting ownership (%i:%i) of %s failed: %s (%i)",
 				ent_pw->pw_uid, ent_pw->pw_gid, FTLfiles.FTL_db, strerror(errno), errno);
 			chown_all_shmem(ent_pw);
 		}
 		else
 		{
-			logg("INFO: FTL is running as root");
+			log_info("FTL is running as root");
 		}
 	}
 	else
@@ -104,10 +104,10 @@ void FTL_fork_and_bind_sockets(struct passwd *ent_pw)
 		uid_t uid;
 		struct passwd *current_user;
 		if ((current_user = getpwuid(uid = geteuid())) != NULL)
-			logg("INFO: FTL is running as user %s (UID %d)",
-			     current_user->pw_name, (int)current_user->pw_uid);
+			log_info("FTL is running as user %s (UID %d)",
+			         current_user->pw_name, (int)current_user->pw_uid);
 		else
-			logg("INFO: Failed to obtain information about FTL user");
+			log_info("Failed to obtain information about FTL's owner");
 	}
 
 	// Obtain DNS port from dnsmasq daemon
