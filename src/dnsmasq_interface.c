449f782bbd2a35cd32cc2ed951e681c86fa9857d
DL6ER <dl6er@dl6er.de>
2021-10-03 14:41:45 +0200

    Improve rate-limiting warninng in pihole.log
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq_interface.c
+++ src/dnsmasq_interface.c
@@ -295,11 +295,12 @@ size_t _FTL_make_answer(struct dns_header *header, char *limit, const size_t len
 	// Setup reply header
 	setup_reply(header, flags, *ede);
 
-	// Add NEG flag when replying with NXDOMAIN. This is necessary to get proper logging in pihole.log
-	// At the same time, we cannot add NEG before calling setup_reply() as it would, otherwise, result
-	// in an incorrect "nowhere to forward to" log entry (because setup_reply() checks for equality of
-	// flags instead of doing a bitmask comparison).
-	if(flags == F_NXDOMAIN)
+	// Add NEG flag when replying with NXDOMAIN or NODATA. This is necessary
+	// to get proper logging in pihole.log At the same time, we cannot add
+	// NEG before calling setup_reply() as it would, otherwise, result in an
+	// incorrect "nowhere to forward to" log entry (because setup_reply()
+	// checks for equality of flags instead of doing a bitmask comparison).
+	if(flags == F_NXDOMAIN || flags == F_NOERR)
 		flags |= F_NEG;
 
 	// Add flags according to current blocking mode
@@ -388,6 +389,7 @@ size_t _FTL_make_answer(struct dns_header *header, char *limit, const size_t len
 		else
 		{
 			// NODATA/NXDOMAIN
+			// gravity blocked abc.com is NODATA/NXDOMAIN
 			log_query(flags, name, NULL, (char*)blockingreason, 0);
 		}
 	}
@@ -622,7 +624,6 @@ bool _FTL_new_query(const unsigned int flags, const char *name,
 		client->flags.rate_limited = true;
 
 		// Block this query
-		blockingreason = "rate-limiting";
 		force_next_DNS_reply = REPLY_REFUSED;
 		blockingreason = "Rate-limiting";
 
