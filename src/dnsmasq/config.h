882563a154ef3c65249f5ef7618f3e2c15b7112e
Dominik Derigs <dl6er@dl6er.de>
2021-12-28 11:03:40 +0100

    Minimum safe size is recommended to be 1232. See https://dnsflagday.net/2020/
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/config.h
+++ src/dnsmasq/config.h
@@ -20,7 +20,7 @@
 #define TCP_MAX_QUERIES 100 /* Maximum number of queries per incoming TCP connection */
 #define TCP_BACKLOG 32  /* kernel backlog limit for TCP connections */
 #define EDNS_PKTSZ 4096 /* default max EDNS.0 UDP packet from RFC5625 */
-#define SAFE_PKTSZ 1280 /* "go anywhere" UDP packet size */
+#define SAFE_PKTSZ 1232 /* "go anywhere" UDP packet size, see https://dnsflagday.net/2020/ */
 #define KEYBLOCK_LEN 40 /* choose to minimise fragmentation when storing DNSSEC keys */
 #define DNSSEC_WORK 50 /* Max number of queries to validate one question */
 #define TIMEOUT 10     /* drop UDP queries after TIMEOUT seconds */
