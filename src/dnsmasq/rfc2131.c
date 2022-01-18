a20aec1fa6e69e6eafd458e07a37b1cacab05401
Simon Kelley <simon@thekelleys.org.uk>
2022-01-18 00:55:13 +0000

    Fix crash in PXE code with bad config.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/rfc2131.c
+++ src/dnsmasq/rfc2131.c
@@ -2200,8 +2200,9 @@ static int pxe_uefi_workaround(int pxe_arch, struct dhcp_netid *netid, struct dh
       inet_ntop(AF_INET, &mess->siaddr, (char *)mess->sname, INET_ADDRSTRLEN);
     }
   
-  snprintf((char *)mess->file, sizeof(mess->file), 
-	   strchr(found->basename, '.') ? "%s" : "%s.0", found->basename);
+  if (found->basename)
+    snprintf((char *)mess->file, sizeof(mess->file), 
+	     strchr(found->basename, '.') ? "%s" : "%s.0", found->basename);
   
   return 1;
 }
