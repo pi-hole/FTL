e6c4c2941715d23bc42a2152552a9a3b939361a8
Johnny S. Lee via Dnsmasq-discuss <dnsmasq-discuss@lists.thekelleys.org.uk>
2022-02-03 23:42:00 +0000

    Fix FTBFS on BSD platforms.
    Bug introduced in fc664d114d6e11ced4912b746f18d543f662066b
    
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/dhcp.c
+++ src/dnsmasq/dhcp.c
@@ -458,9 +458,12 @@ void dhcp_packet(time_t now, int pxe_fd)
   else 
     {
 #ifdef HAVE_DUMPFILE
-      dest.sin_addr.s_addr = (ntohs(mess->flags) & 0x8000) ? INADDR_BROADCAST : mess->yiaddr;
+      if (ntohs(mess->flags) & 0x8000)
+        dest.sin_addr.s_addr = INADDR_BROADCAST;
+      else
+        dest.sin_addr = mess->yiaddr;
       dest.sin_port = htons(daemon->dhcp_client_port);
-
+      
       dump_packet(DUMP_DHCP, (void *)iov.iov_base, iov.iov_len, NULL,
 		  (union mysockaddr *)&dest, daemon->dhcp_server_port);
 #endif
