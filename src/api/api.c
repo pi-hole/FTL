f4a14eb4958a6747e2cbc82c81e423111d899e9c
DL6ER <dl6er@dl6er.de>
2022-05-01 20:17:16 +0200

    Also always send all information for Client activity over last 24 hours API call
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/api/api.c
+++ src/api/api.c
@@ -1225,35 +1225,10 @@ void getDBstats(const int *sock)
 
 void getClientsOverTime(const int *sock)
 {
-	int sendit = -1, until = OVERTIME_SLOTS;
-
 	// Exit before processing any data if requested via config setting
 	get_privacy_level(NULL);
 	if(config.privacylevel >= PRIVACY_HIDE_DOMAINS_CLIENTS)
 		return;
-
-	// Find minimum ID to send
-	for(int slot = 0; slot < OVERTIME_SLOTS; slot++)
-	{
-		if((overTime[slot].total > 0 || overTime[slot].blocked > 0) &&
-		   overTime[slot].timestamp >= overTime[0].timestamp)
-		{
-			sendit = slot;
-			break;
-		}
-	}
-	if(sendit < 0)
-		return;
-
-	// Find minimum ID to send
-	for(int slot = 0; slot < OVERTIME_SLOTS; slot++)
-	{
-		if(overTime[slot].timestamp >= time(NULL))
-		{
-			until = slot;
-			break;
-		}
-	}
 
 	// Get clients which the user doesn't want to see
 	char * excludeclients = read_setupVarsconf("API_EXCLUDE_CLIENTS");
@@ -1284,7 +1259,7 @@ void getClientsOverTime(const int *sock)
 	}
 
 	// Main return loop
-	for(int slot = sendit; slot < until; slot++)
+	for(int slot = 0; slot < OVERTIME_SLOTS; slot++)
 	{
 		if(istelnet[*sock])
 			ssend(*sock, "%lli", (long long)overTime[slot].timestamp);
