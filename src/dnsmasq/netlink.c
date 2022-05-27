1aa4087abe81157be31efc0b26422a79ce43c9fb
Beniamino Galvani <bgalvani@redhat.com>
2022-05-27 21:16:18 +0100

    Fix parsing of IPv6 addresses with peer from netlink.
    In the most common case, an IPv6 address doesn't have a peer and the
    IFA_ADDRESS netlink attribute contains the address itself.
    
    But if the address has a peer (typically for point to point links),
    then IFA_ADDRESS contains the peer address and IFA_LOCAL contains the
    address [1].
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/net/ipv6/addrconf.c?h=v5.17#n5030
    
    Fix the parsing of IPv6 addresses with peers, as currently dnsmasq
    unsuccessfully tries to bind on the peer address.
    
    A simple reproducer is:
    
      dnsmasq --conf-file=/dev/null -i dummy1 -d --bind-dynamic &
      sleep 2
      ip link add dummy1 type dummy
      ip link set dummy1 up
      ip addr add dev dummy1 fd01::1/64 peer fd01::2/64
      ip addr add dev dummy1 fd01::42/64
      sleep 2
      ss -lnp | grep dnsmasq | grep fd01
    
    Before the patch:
      dnsmasq: failed to create listening socket for fd01::2: Cannot assign requested address
      dnsmasq: failed to create listening socket for fd01::2: Cannot assign requested address
      udp   UNCONN 0   [fd01::42]:53   [::]:*    users:(("dnsmasq",pid=23947,fd=14))
      tcp   LISTEN 0   [fd01::42]:53   [::]:*    users:(("dnsmasq",pid=23947,fd=15
    
    After:
      udp   UNCONN 0   [fd01::42]:53   [::]:*    users:(("dnsmasq",pid=23973,fd=16))
      udp   UNCONN 0    [fd01::1]:53   [::]:*    users:(("dnsmasq",pid=23973,fd=14))
      tcp   LISTEN 0   [fd01::42]:53   [::]:*    users:(("dnsmasq",pid=23973,fd=17))
      tcp   LISTEN 0    [fd01::1]:53   [::]:*    users:(("dnsmasq",pid=23973,fd=15))
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/netlink.c
+++ src/dnsmasq/netlink.c
@@ -258,7 +258,16 @@ int iface_enumerate(int family, void *parm, int (*callback)())
 		    
 		    while (RTA_OK(rta, len1))
 		      {
-			if (rta->rta_type == IFA_ADDRESS)
+			/*
+			 * Important comment: (from if_addr.h)
+			 * IFA_ADDRESS is prefix address, rather than local interface address.
+			 * It makes no difference for normally configured broadcast interfaces,
+			 * but for point-to-point IFA_ADDRESS is DESTINATION address,
+			 * local address is supplied in IFA_LOCAL attribute.
+			 */
+			if (rta->rta_type == IFA_LOCAL)
+			  addrp = ((struct in6_addr *)(rta+1));
+			else if (rta->rta_type == IFA_ADDRESS && !addrp)
 			  addrp = ((struct in6_addr *)(rta+1)); 
 			else if (rta->rta_type == IFA_CACHEINFO)
 			  {
