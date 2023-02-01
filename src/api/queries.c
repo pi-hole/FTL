65a31f0241ba9c0533ff7d01227fd765dbe87822
DL6ER <dl6er@dl6er.de>
2023-02-01 22:21:05 +0100

    Improvements for the query log filter suggestions (increase default number of suggestions from 10 to 30 and differentiate between clients by IP and by name)
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/api/queries.c
+++ src/api/queries.c
@@ -74,7 +74,7 @@ int api_queries_suggestions(struct ftl_conn *api)
 {
 	int rc;
 	// Does the user request a custom number of records to be included?
-	int count = 10;
+	int count = 30;
 	get_int_var(api->request->query_string, "count", &count);
 
 	// Lock shared memory
@@ -94,22 +94,24 @@ int api_queries_suggestions(struct ftl_conn *api)
 	// Get clients, both by IP and names
 	// We have to call DISTINCT() here as multiple IPs can map to and name and
 	// vice versa
-	cJSON *client = JSON_NEW_ARRAY();
-	rc = add_strings_to_array(api, client, "SELECT DISTINCT(ip) FROM client_by_id", count/2);
+	cJSON *client_ip = JSON_NEW_ARRAY();
+	rc = add_strings_to_array(api, client_ip, "SELECT DISTINCT(ip) FROM client_by_id", count);
 	if(rc != 0)
 	{
 		log_err("Cannot read client IPs from database");
 		cJSON_Delete(domain);
-		cJSON_Delete(client);
+		cJSON_Delete(client_ip);
 		unlock_shm();
 		return rc;
 	}
-	rc = add_strings_to_array(api, client, "SELECT DISTINCT(name) FROM client_by_id", count/2);
+	cJSON *client_name = JSON_NEW_ARRAY();
+	rc = add_strings_to_array(api, client_name, "SELECT DISTINCT(name) FROM client_by_id", count);
 	if(rc != 0)
 	{
 		log_err("Cannot read client names from database");
 		cJSON_Delete(domain);
-		cJSON_Delete(client);
+		cJSON_Delete(client_ip);
+		cJSON_Delete(client_name);
 		unlock_shm();
 		return rc;
 	}
@@ -121,7 +123,8 @@ int api_queries_suggestions(struct ftl_conn *api)
 	{
 		log_err("Cannot read forward from database");
 		cJSON_Delete(domain);
-		cJSON_Delete(client);
+		cJSON_Delete(client_ip);
+		cJSON_Delete(client_name);
 		cJSON_Delete(upstream);
 		unlock_shm();
 		return rc;
@@ -166,7 +169,8 @@ int api_queries_suggestions(struct ftl_conn *api)
 
 	cJSON *suggestions = JSON_NEW_OBJECT();
 	JSON_ADD_ITEM_TO_OBJECT(suggestions, "domain", domain);
-	JSON_ADD_ITEM_TO_OBJECT(suggestions, "client", client);
+	JSON_ADD_ITEM_TO_OBJECT(suggestions, "client_ip", client_ip);
+	JSON_ADD_ITEM_TO_OBJECT(suggestions, "client_name", client_name);
 	JSON_ADD_ITEM_TO_OBJECT(suggestions, "upstream", upstream);
 	JSON_ADD_ITEM_TO_OBJECT(suggestions, "type", type);
 	JSON_ADD_ITEM_TO_OBJECT(suggestions, "status", status);
@@ -178,8 +182,10 @@ int api_queries_suggestions(struct ftl_conn *api)
 	JSON_SEND_OBJECT_UNLOCK(json);
 }
 
-#define QUERYSTR "SELECT q.id,timestamp,type,status,domain,client,forward,additional_info,reply_type,reply_time,dnssec,c.name,c.ip" // ttl, regex_id
-#define JOINSTR "JOIN client_by_id c ON q.client = c.id"
+#define QUERYSTR "SELECT q.id,timestamp,q.type,status,d.domain,f.forward,additional_info,reply_type,reply_time,dnssec,c.ip,c.name,a.content" // ttl, regex_id
+// JOIN: Only return rows where there is a match in BOTH tables
+// LEFT JOIN: Return all rows from the left table, and the matched rows from the right table
+#define JOINSTR "JOIN client_by_id c ON q.client = c.id JOIN domain_by_id d ON q.domain = d.id LEFT JOIN forward_by_id f ON q.forward = f.id LEFT JOIN addinfo_by_id a ON a.id = q.additional_info"
 #define QUERYSTRORDER "ORDER BY q.id DESC"
 #define QUERYSTRBUFFERLEN 4096
 static void add_querystr_double(struct ftl_conn *api, char *querystr, const char *sql, const char *uripart, bool *where)
@@ -199,7 +205,7 @@ static void add_querystr_string(struct ftl_conn *api, char *querystr, const char
 	const size_t strpos = strlen(querystr);
 	const char *glue = *where ? "AND" : "WHERE";
 	*where = true;
-	snprintf(querystr + strpos, QUERYSTRBUFFERLEN - strpos, " %s %s%s", glue, sql, val);
+	snprintf(querystr + strpos, QUERYSTRBUFFERLEN - strpos, " %s (%s%s)", glue, sql, val);
 }
 
 static void querystr_finish(char *querystr)
@@ -233,10 +239,11 @@ int api_queries(struct ftl_conn *api)
 
 	// Start building database query string
 	char querystr[QUERYSTRBUFFERLEN] = { 0 };
-	sprintf(querystr, "%s FROM %s q %s", QUERYSTR, disk ? "disk.queries" : "queries", JOINSTR);
+	sprintf(querystr, "%s FROM %s q %s", QUERYSTR, disk ? "disk.query_storage" : "query_storage", JOINSTR);
 	int draw = 0;
 
 	char domainname[512] = { 0 };
+	char clientip[512] = { 0 };
 	char clientname[512] = { 0 };
 	char upstreamname[256] = { 0 };
 	char typename[32] = { 0 };
@@ -267,21 +274,28 @@ int api_queries(struct ftl_conn *api)
 		if(GET_VAR("domain", buffer, api->request->query_string) > 0)
 		{
 			sscanf(buffer, "%255s", domainname);
-			add_querystr_string(api, querystr, "domain=", ":domain", &where);
+			add_querystr_string(api, querystr, "d.domain=", ":domain", &where);
 		}
 
 		// Upstream filtering?
 		if(GET_VAR("upstream", buffer, api->request->query_string) > 0)
 		{
 			sscanf(buffer, "%255s", upstreamname);
-			add_querystr_string(api, querystr, "forward=", ":upstream", &where);
+			add_querystr_string(api, querystr, "f.forward=", ":upstream", &where);
+		}
+
+		// Client IP filtering?
+		if(GET_VAR("client_ip", buffer, api->request->query_string) > 0)
+		{
+			sscanf(buffer, "%255s", clientip);
+			add_querystr_string(api, querystr, "c.ip=", ":cip", &where);
 		}
 
 		// Client filtering?
-		if(GET_VAR("client", buffer, api->request->query_string) > 0)
+		if(GET_VAR("client_name", buffer, api->request->query_string) > 0)
 		{
 			sscanf(buffer, "%255s", clientname);
-			add_querystr_string(api, querystr, "client=", ":client", &where);
+			add_querystr_string(api, querystr, "c.name=", ":cname", &where);
 		}
 
 		// DataTables server-side processing protocol
@@ -389,37 +403,64 @@ int api_queries(struct ftl_conn *api)
 	{
 		int idx;
 		idx = sqlite3_bind_parameter_index(read_stmt, ":domain");
-		if(idx > 0 && (rc = sqlite3_bind_text(read_stmt, idx, domainname, -1, SQLITE_STATIC)) != SQLITE_OK)
+		if(idx > 0)
 		{
-			sqlite3_reset(read_stmt);
-			sqlite3_finalize(read_stmt);
-			unlock_shm();
-			return send_json_error(api, 500,
-			                       "internal_error",
-			                       "Internal server error, failed to bind domain to SQL query",
-			                       sqlite3_errstr(rc));
+			log_debug(DEBUG_API, "adding :domain = \"%s\" to query", domainname);
+			if((rc = sqlite3_bind_text(read_stmt, idx, domainname, -1, SQLITE_STATIC)) != SQLITE_OK)
+			{
+				sqlite3_reset(read_stmt);
+				sqlite3_finalize(read_stmt);
+				unlock_shm();
+				return send_json_error(api, 500,
+				                       "internal_error",
+				                       "Internal server error, failed to bind domain to SQL query",
+				                       sqlite3_errstr(rc));
+			}
 		}
-		idx = sqlite3_bind_parameter_index(read_stmt, ":client");
-		if(idx > 0 && (rc = sqlite3_bind_text(read_stmt, idx, clientname, -1, SQLITE_STATIC)) != SQLITE_OK)
+		idx = sqlite3_bind_parameter_index(read_stmt, ":cip");
+		if(idx > 0)
 		{
-			sqlite3_reset(read_stmt);
-			sqlite3_finalize(read_stmt);
-			unlock_shm();
-			return send_json_error(api, 500,
-			                       "internal_error",
-			                       "Internal server error, failed to bind client to SQL query",
-			                       sqlite3_errstr(rc));
+			log_debug(DEBUG_API, "adding :cip = \"%s\" to query", clientip);
+			if((rc = sqlite3_bind_text(read_stmt, idx, clientip, -1, SQLITE_STATIC)) != SQLITE_OK)
+			{
+				sqlite3_reset(read_stmt);
+				sqlite3_finalize(read_stmt);
+				unlock_shm();
+				return send_json_error(api, 500,
+				                       "internal_error",
+				                       "Internal server error, failed to bind cip to SQL query",
+				                       sqlite3_errstr(rc));
+			}
+		}
+		idx = sqlite3_bind_parameter_index(read_stmt, ":cname");
+		if(idx > 0)
+		{
+			log_debug(DEBUG_API, "adding :cname = \"%s\" to query", clientname);
+			if((rc = sqlite3_bind_text(read_stmt, idx, clientname, -1, SQLITE_STATIC)) != SQLITE_OK)
+			{
+				sqlite3_reset(read_stmt);
+				sqlite3_finalize(read_stmt);
+				unlock_shm();
+				return send_json_error(api, 500,
+				                       "internal_error",
+				                       "Internal server error, failed to bind client to SQL query",
+				                       sqlite3_errstr(rc));
+			}
 		}
 		idx = sqlite3_bind_parameter_index(read_stmt, ":upstream");
-		if(idx > 0 && (rc = sqlite3_bind_text(read_stmt, idx, upstreamname, -1, SQLITE_STATIC)) != SQLITE_OK)
+		if(idx > 0)
 		{
-			sqlite3_reset(read_stmt);
-			sqlite3_finalize(read_stmt);
-			unlock_shm();
-			return send_json_error(api, 500,
-			                       "internal_error",
-			                       "Internal server error, failed to bind upstream to SQL query",
-			                       sqlite3_errstr(rc));
+			log_debug(DEBUG_API, "adding :upstream = \"%s\" to query", upstreamname);
+			if((rc = sqlite3_bind_text(read_stmt, idx, upstreamname, -1, SQLITE_STATIC)) != SQLITE_OK)
+			{
+				sqlite3_reset(read_stmt);
+				sqlite3_finalize(read_stmt);
+				unlock_shm();
+				return send_json_error(api, 500,
+				                       "internal_error",
+				                       "Internal server error, failed to bind upstream to SQL query",
+				                       sqlite3_errstr(rc));
+			}
 		}
 		idx = sqlite3_bind_parameter_index(read_stmt, ":type");
 		if(idx > 0)
@@ -432,6 +473,7 @@ int api_queries(struct ftl_conn *api)
 			}
 			if(type < TYPE_MAX)
 			{
+				log_debug(DEBUG_API, "adding :type = %d to query", type);
 				rc = sqlite3_bind_int(read_stmt, idx, type);
 				if(rc != SQLITE_OK)
 				{
@@ -464,6 +506,7 @@ int api_queries(struct ftl_conn *api)
 			}
 			if(status <QUERY_STATUS_MAX)
 			{
+				log_debug(DEBUG_API, "adding :status = %d to query", status);
 				rc = sqlite3_bind_int(read_stmt, idx, status);
 				if(rc != SQLITE_OK)
 				{
@@ -496,6 +539,7 @@ int api_queries(struct ftl_conn *api)
 			}
 			if(reply <QUERY_REPLY_MAX)
 			{
+				log_debug(DEBUG_API, "adding :reply = %d to query", reply);
 				rc = sqlite3_bind_int(read_stmt, idx, reply);
 				if(rc != SQLITE_OK)
 				{
@@ -528,6 +572,7 @@ int api_queries(struct ftl_conn *api)
 			}
 			if(dnssec < DNSSEC_MAX)
 			{
+				log_debug(DEBUG_API, "adding :dnssec = %d to query", dnssec);
 				rc = sqlite3_bind_int(read_stmt, idx, dnssec);
 				if(rc != SQLITE_OK)
 				{
@@ -561,7 +606,7 @@ int api_queries(struct ftl_conn *api)
 	while((rc = sqlite3_step(read_stmt)) == SQLITE_ROW)
 	{
 		// Get ID of query from database
-		id = sqlite3_column_int64(read_stmt, 0);
+		id = sqlite3_column_int64(read_stmt, 0); // q.id
 
 		// Set firstID from the first returned value
 		if(firstID == -1)
@@ -574,13 +619,11 @@ int api_queries(struct ftl_conn *api)
 		if((unsigned long)id > cursor)
 		{
 			// Skip all results with id BEFORE cursor (static tip of table)
-			log_info("Skipping BEFORE 1");
 			continue;
 		}
 		else if(start > 0 && start >= records)
 		{
 			// Skip all results BEFORE start (server-side pagination)
-			log_info("Skipping BEFORE 2");
 			continue;
 		}
 		else if(length > 0 && added >= (unsigned int)length)
@@ -589,59 +632,87 @@ int api_queries(struct ftl_conn *api)
 			// everything.
 			// Skip everything AFTER we added the requested number
 			// of queries if length is > 0.
-			log_info("Skipping BEFORE 3");
-			continue;
+			break;
 		}
-		log_info("Processing");
 
 		// Build item object
 		cJSON *item = JSON_NEW_OBJECT();
 		queriesData query = { 0 };
 		char buffer[20] = { 0 };
 		JSON_ADD_NUMBER_TO_OBJECT(item, "id", id);
-		JSON_ADD_NUMBER_TO_OBJECT(item, "time", sqlite3_column_double(read_stmt, 1));
-		query.type = sqlite3_column_int(read_stmt, 2);
-		query.status = sqlite3_column_int(read_stmt, 3);
-		query.reply = sqlite3_column_int(read_stmt, 8);
-		query.dnssec = sqlite3_column_int(read_stmt, 9);
+		JSON_ADD_NUMBER_TO_OBJECT(item, "time", sqlite3_column_double(read_stmt, 1)); // timestamp
+		query.type = sqlite3_column_int(read_stmt, 2); // type
+		query.status = sqlite3_column_int(read_stmt, 3); // status
+		query.reply = sqlite3_column_int(read_stmt, 7); // reply_type
+		query.dnssec = sqlite3_column_int(read_stmt, 9); // dnssec
 		// We have to copy the string as TYPExxx string won't be static
 		JSON_COPY_STR_TO_OBJECT(item, "type", get_query_type_str(query.type, &query, buffer));
 		JSON_REF_STR_IN_OBJECT(item, "status", get_query_status_str(query.status));
 		JSON_REF_STR_IN_OBJECT(item, "dnssec", get_query_dnssec_str(query.dnssec));
-		JSON_COPY_STR_TO_OBJECT(item, "domain", sqlite3_column_text(read_stmt, 4));
-		if(sqlite3_column_type(read_stmt, 6) == SQLITE_NULL)
-		{
-			JSON_ADD_NULL_TO_OBJECT(item, "upstream");
-		}
+		JSON_COPY_STR_TO_OBJECT(item, "domain", sqlite3_column_text(read_stmt, 4)); // d.domain
+
+		if(sqlite3_column_type(read_stmt, 5) == SQLITE_TEXT &&
+		   sqlite3_column_bytes(read_stmt, 5) > 0)
+			JSON_COPY_STR_TO_OBJECT(item, "upstream", sqlite3_column_text(read_stmt, 5)); // f.forward
 		else
-		{
-			JSON_COPY_STR_TO_OBJECT(item, "upstream", sqlite3_column_text(read_stmt, 6));
-		}
+			JSON_ADD_NULL_TO_OBJECT(item, "upstream");
 
 		cJSON *reply = JSON_NEW_OBJECT();
 		JSON_REF_STR_IN_OBJECT(reply, "type", get_query_reply_str(query.reply));
-		JSON_ADD_NUMBER_TO_OBJECT(reply, "time", sqlite3_column_double(read_stmt, 10));
+		JSON_ADD_NUMBER_TO_OBJECT(reply, "time", sqlite3_column_double(read_stmt, 8)); // reply_time
 		JSON_ADD_ITEM_TO_OBJECT(item, "reply", reply);
 
 		cJSON *client = JSON_NEW_OBJECT();
-		JSON_COPY_STR_TO_OBJECT(client, "ip", sqlite3_column_text(read_stmt, 5));
-		if(sqlite3_column_type(read_stmt, 11) == SQLITE_TEXT)
-		{
-			JSON_COPY_STR_TO_OBJECT(client, "name", sqlite3_column_text(read_stmt, 11));
-		}
+		JSON_COPY_STR_TO_OBJECT(client, "ip", sqlite3_column_text(read_stmt, 10)); // c.ip
+
+		if(sqlite3_column_type(read_stmt, 11) == SQLITE_TEXT &&
+		   sqlite3_column_bytes(read_stmt, 11) > 0)
+			JSON_COPY_STR_TO_OBJECT(client, "name", sqlite3_column_text(read_stmt, 11)); // c.name
 		else
-		{
 			JSON_ADD_NULL_TO_OBJECT(client, "name");
-		}
 		JSON_ADD_ITEM_TO_OBJECT(item, "client", client);
 
-		JSON_ADD_NUMBER_TO_OBJECT(item, "ttl", sqlite3_column_int(read_stmt, 12));
-		JSON_ADD_NUMBER_TO_OBJECT(item, "regex_id", sqlite3_column_int(read_stmt, 13));
+		JSON_ADD_NUMBER_TO_OBJECT(item, "ttl", 0); // sqlite3_column_int(read_stmt, 12));
+		JSON_ADD_NUMBER_TO_OBJECT(item, "regex_id", 0); // sqlite3_column_int(read_stmt, 13));
+
+		const unsigned char *cname = NULL;
+		switch(query.status)
+		{
+			case QUERY_GRAVITY_CNAME:
+			case QUERY_REGEX_CNAME:
+			case QUERY_DENYLIST_CNAME:
+			{
+				cname = sqlite3_column_text(read_stmt, 12);
+				break;
+			}
+			case QUERY_UNKNOWN:
+			case QUERY_GRAVITY:
+			case QUERY_FORWARDED:
+			case QUERY_CACHE:
+			case QUERY_REGEX:
+			case QUERY_DENYLIST:
+			case QUERY_EXTERNAL_BLOCKED_IP:
+			case QUERY_EXTERNAL_BLOCKED_NULL:
+			case QUERY_EXTERNAL_BLOCKED_NXRA:
+			case QUERY_RETRIED:
+			case QUERY_RETRIED_DNSSEC:
+			case QUERY_IN_PROGRESS:
+			case QUERY_DBBUSY:
+			case QUERY_SPECIAL_DOMAIN:
+			case QUERY_CACHE_STALE:
+			case QUERY_STATUS_MAX:
+				break;
+		}
+		if(cname != NULL)
+			JSON_COPY_STR_TO_OBJECT(item, "cname", (const char*)cname);
+		else
+			JSON_ADD_NULL_TO_OBJECT(item, "cname");
 
 		JSON_ADD_ITEM_TO_ARRAY(queries, item);
 
 		added++;
 	}
+	log_debug(DEBUG_API, "Sending %u of %lu in memory and %lu on disk queries", added, mem_dbnum, disk_dbnum);
 	cJSON *json = JSON_NEW_OBJECT();
 	JSON_ADD_ITEM_TO_OBJECT(json, "queries", queries);
 
@@ -649,6 +720,7 @@ int api_queries(struct ftl_conn *api)
 	{
 		// Repeat cursor received in the request. This ensures we get a
 		// static result by skipping any newer queries.
+		log_debug(DEBUG_API, "Sending cursor %lu", cursor);
 		JSON_ADD_NUMBER_TO_OBJECT(json, "cursor", cursor);
 	}
 	else
@@ -656,6 +728,7 @@ int api_queries(struct ftl_conn *api)
 		// Send cursor pointing to the firstID of the data obtained in
 		// this query. This ensures we get a static result by skipping
 		// any newer queries.
+		log_debug(DEBUG_API, "Sending cursor %lli (firstID)", (long long int)firstID);
 		JSON_ADD_NUMBER_TO_OBJECT(json, "cursor", firstID);
 	}
 
@@ -663,6 +736,9 @@ int api_queries(struct ftl_conn *api)
 	JSON_ADD_NUMBER_TO_OBJECT(json, "recordsTotal", disk ? disk_dbnum : mem_dbnum);
 	JSON_ADD_NUMBER_TO_OBJECT(json, "recordsFiltered", records);
 	JSON_ADD_NUMBER_TO_OBJECT(json, "draw", draw);
+
+	// Finalize statements
+	sqlite3_finalize(read_stmt);
 
 	if(disk && !detach_disk_database(&message))
 	{
