a1332582ff8683028283c1b895b15243155e9fde
DL6ER <dl6er@dl6er.de>
2023-04-01 14:47:45 +0200

    Use new shortcut cache-rr=ANY to enable caching of all query types
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/config/dnsmasq_config.c
+++ src/config/dnsmasq_config.c
@@ -507,16 +507,9 @@ bool __attribute__((const)) write_dnsmasq_config(struct config *conf, bool test_
 		fputs("\n", pihole_conf);
 	}
 
-	// Add option for caching all names RRNAMEs that are not already cached
-	// by default
-	fputs("# Cache all names RRNAMEs that are not already cached by default\n", pihole_conf);
-	fputs("cache-rr=NS,MD,MF,SOA,MB,MG,MR,NULL,WKS,PTR,HINFO,MINFO,MX,TXT,RP,"
-	      "AFSDB,X25,ISDN,RT,NSAP,NSAP_PTR,SIG,KEY,PX,GPOS,LOC,NXT,EID,NIMLOC,"
-	      "ATMA,NAPTR,KX,CERT,A6,DNAME,SINK,OPT,APL,DS,SSHFP,IPSECKEY,RRSIG,NSEC,"
-	      "DNSKEY,DHCID,NSEC3,NSEC3PARAM,TLSA,SMIMEA,HIP,NINFO,RKEY,TALINK,CDS,"
-	      "CDNSKEY,OPENPGPKEY,CSYNC,ZONEMD,SVCB,HTTPS,SPF,UINFO,UID,GID,UNSPEC,"
-	      "NID,L32,L64,LP,EUI48,EUI64,TKEY,TSIG,IXFR,AXFR,MAILB,MAILA,ANY,URI,"
-	      "CAA,AVC,DOA,AMTRELAY,TA,DLV\n", pihole_conf);
+	// Add option for caching all DNS records
+	fputs("# Cache all DNS records\n", pihole_conf);
+	fputs("cache-rr=ANY\n", pihole_conf);
 
 	// Flush config file to disk
 	fflush(pihole_conf);
