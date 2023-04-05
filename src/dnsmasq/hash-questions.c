e2f7ae69141ef4e1b7160741958cc4b657cf6dbd
Simon Kelley <simon@thekelleys.org.uk>
2023-04-05 17:42:23 +0100

    Missed copyright date.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/hash-questions.c
+++ src/dnsmasq/hash-questions.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2020 Simon Kelley
+/* Copyright (c) 2012-2023 Simon Kelley
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
