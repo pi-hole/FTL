a02f4eb42a88adbf43c7fda83be7e7967fb8d462
DL6ER <dl6er@dl6er.de>
2023-05-22 19:13:17 +0200

    Add new Kepler syntax commit to Civetweb-related patch series
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- patch/civetweb.sh
+++ patch/civetweb.sh
@@ -3,5 +3,6 @@ set -e
 
 patch -p1 < patch/civetweb/0001-add-pihole-mods.patch
 patch -p1 < patch/civetweb/0001-Add-NO_DLOPEN-option-to-civetweb-s-LUA-routines.patch
+patch -p1 < patch/civetweb/0001-Always-Kepler-syntax-for-Lua-server-pages.patch
 
 echo "ALL PATCHES APPLIED OKAY"
