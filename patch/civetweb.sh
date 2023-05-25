182a10701b2249ca14b99b8859385ed26d33c666
DL6ER <dl6er@dl6er.de>
2023-05-25 18:04:01 +0200

    Add new CivetWeb patch needed for URL rewriting
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- patch/civetweb.sh
+++ patch/civetweb.sh
@@ -4,5 +4,6 @@ set -e
 patch -p1 < patch/civetweb/0001-add-pihole-mods.patch
 patch -p1 < patch/civetweb/0001-Add-NO_DLOPEN-option-to-civetweb-s-LUA-routines.patch
 patch -p1 < patch/civetweb/0001-Always-Kepler-syntax-for-Lua-server-pages.patch
+patch -p1 < patch/civetweb/0001-Add-FTL-URI-rewriting-changes-to-CivetWeb.patch
 
 echo "ALL PATCHES APPLIED OKAY"
