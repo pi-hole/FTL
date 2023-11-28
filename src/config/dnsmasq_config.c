01af195c776367a5931debb103a07b2b7baf1628
DL6ER <DL6ER@users.noreply.github.com>
2023-11-28 22:16:38 +0100

    Update src/config/dnsmasq_config.c
    Co-authored-by: yubiuser <ckoenig@posteo.de>
    Signed-off-by: DL6ER <DL6ER@users.noreply.github.com>

--- src/config/dnsmasq_config.c
+++ src/config/dnsmasq_config.c
@@ -411,6 +411,7 @@ bool __attribute__((const)) write_dnsmasq_config(struct config *conf, bool test_
 	if(conf->dns.cache.optimizer.v.i > -1)
 	{
 		fputs("# Use stale cache entries for a given number of seconds to optimize cache utilization\n", pihole_conf);
+		fputs("# Setting the time to zero will serve stale cache data regardless how long it has expired.\n", pihole_conf);
 		fprintf(pihole_conf, "use-stale-cache=%i\n", conf->dns.cache.optimizer.v.i);
 		fputs("\n", pihole_conf);
 	}
