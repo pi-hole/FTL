a2df36ec47d57a58334b6530603a91c20e2e58b7
DL6ER <dl6er@dl6er.de>
2023-12-07 22:48:11 +0100

    Add missing legacy config import code for NICE -> misc.nice
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/config/legacy_reader.c
+++ src/config/legacy_reader.c
@@ -337,6 +337,10 @@ const char *readFTLlegacy(struct config *conf)
 	// 2.0) had the range -infinity..15.
 	buffer = parseFTLconf(fp, "NICE");
 
+	value = 0;
+	if(buffer != NULL && sscanf(buffer, "%i", &value) && value >= -20 && value <= 19)
+		conf->misc.nice.v.i = value;
+
 	// MAXNETAGE
 	// IP addresses (and associated host names) older than the specified number
 	// of days are removed to avoid dead entries in the network overview table
