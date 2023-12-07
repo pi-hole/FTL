dae83272775525be5461047d2acfd3d67a10c29f
DL6ER <dl6er@dl6er.de>
2023-12-07 22:50:29 +0100

    Password-related code improvements and fixes
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/config/password.c
+++ src/config/password.c
@@ -91,6 +91,11 @@ static char * __attribute__((malloc)) base64_encode(const uint8_t *data, const s
 	out_len = base64_encode_update(&ctx, encoded, length, data);
 	out_len += base64_encode_final(&ctx, encoded + out_len);
 
+	// Lenght check
+	if(out_len > BASE64_ENCODE_LENGTH(length) + BASE64_ENCODE_FINAL_LENGTH)
+		log_warn("Base64 encoding may have failed: Output buffer too small? (%zu > %zu)",
+		         out_len, BASE64_ENCODE_LENGTH(length) + BASE64_ENCODE_FINAL_LENGTH);
+
 	return encoded;
 }
 
@@ -259,7 +264,7 @@ static bool parse_PHC_string(const char *phc, size_t *s_cost, size_t *t_cost, ui
 	// Decode salt and hash
 	size_t salt_len = 0;
 	*salt = base64_decode(salt_base64, &salt_len);
-	if(salt == NULL)
+	if(*salt == NULL)
 	{
 		// Error
 		log_err("Error while decoding salt: %s", strerror(errno));
@@ -275,7 +280,7 @@ static bool parse_PHC_string(const char *phc, size_t *s_cost, size_t *t_cost, ui
 
 	size_t hash_len = 0;
 	*hash = base64_decode(hash_base64, &hash_len);
-	if(hash == NULL)
+	if(*hash == NULL)
 	{
 		// Error
 		log_err("Error while decoding hash: %s", strerror(errno));
@@ -376,10 +381,8 @@ enum password_result verify_password(const char *password, const char *pwhash, c
 
 		// Free allocated memory
 		free(supplied);
-		if(salt != NULL)
-			free(salt);
-		if(config_hash != NULL)
-			free(config_hash);
+		free(salt);
+		free(config_hash);
 
 		// Successful logins do not count against rate-limiting
 		if(result)
@@ -408,11 +411,10 @@ enum password_result verify_password(const char *password, const char *pwhash, c
 				writeFTLtoml(true);
 				free(new_hash);
 			}
-		}
 
-		// Successful logins do not count against rate-limiting
-		if(result)
+			// Successful logins do not count against rate-limiting
 			num_password_attempts--;
+		}
 
 		return result ? PASSWORD_CORRECT : PASSWORD_INCORRECT;
 	}
