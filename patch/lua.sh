40cfbbbbff0b95d49e2e544bfc088e9fc0ce441b
DL6ER <dl6er@dl6er.de>
2024-06-26 08:34:02 +0200

    Add previous commit to LUA patch series
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- patch/lua.sh
+++ patch/lua.sh
@@ -3,5 +3,6 @@ set -e
 
 patch -p1 < patch/lua/0001-add-pihole-library.patch
 patch -p1 < patch/lua/0002-Make-lsqlite3-globally-available-in-LUA.patch
+patch -p1 < patch/lua/0003-Make-Pi-hole-bundled-scripts-e.g.-inspect-also-avail.patch
 
 echo "ALL PATCHES APPLIED OKAY"
