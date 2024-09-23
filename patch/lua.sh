59cfeb4ed51560ec9ef12fc10784d192b6aa90e0
DL6ER <dl6er@dl6er.de>
2024-09-23 13:26:24 +0200

    Add new Lua patch
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- patch/lua.sh
+++ patch/lua.sh
@@ -2,5 +2,6 @@
 set -e
 
 patch -p1 < patch/lua/0001-add-pihole-library.patch
+patch -p1 < patch/lua/0001-Increase-LUA_IDSIZE-so-that-long-script-filenames-as.patch
 
 echo "ALL PATCHES APPLIED OKAY"
