066f2d4f7900b28982c87cc1440203ce295f12bb
Simon Kelley <simon@thekelleys.org.uk>
2024-11-02 22:13:52 +0000

    Don't send suspect answer as query to next server in DNS TCP codepath.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/forward.c
+++ src/dnsmasq/forward.c
@@ -1085,8 +1085,7 @@ static void dnssec_validate(struct frec *forward, struct dns_header *header,
 		  new->dependent = forward;
 		  /* Make consistent, only replace query copy with unvalidated answer
 		     when we set ->blocking_query. */
-		  if (forward->stash)
-		    blockdata_free(forward->stash);
+		  blockdata_free(forward->stash);
 		  forward->blocking_query = new;
 		  forward->stash_len = plen;
 		  forward->stash = stash;
@@ -1135,8 +1134,7 @@ static void dnssec_validate(struct frec *forward, struct dns_header *header,
 		  /* Make consistent, only replace query copy with unvalidated answer
 		     when we set ->blocking_query. */
 		  forward->blocking_query = new; 
-		  if (forward->stash)
-		    blockdata_free(forward->stash);
+		  blockdata_free(forward->stash);
 		  forward->stash_len = plen;
 		  forward->stash = stash;
 		  
@@ -2067,6 +2065,7 @@ static ssize_t tcp_talk(int first, int last, int start, unsigned char *packet,  
   unsigned int rsize;
   int class, rclass, type, rtype;
   unsigned char *p;
+  struct blockdata *saved_question;
   
   (void)mark;
   (void)have_mark;
@@ -2077,7 +2076,11 @@ static ssize_t tcp_talk(int first, int last, int start, unsigned char *packet,  
     return 0;
   GETSHORT(type, p); 
   GETSHORT(class, p);
-    
+
+  /* Save question for retry. */
+  if (!(saved_question = blockdata_alloc((char *)header, (size_t)qsize)))
+    return 0;
+  
   while (1) 
     {
       int data_sent = 0, timedout = 0;
@@ -2099,6 +2102,8 @@ static ssize_t tcp_talk(int first, int last, int start, unsigned char *packet,  
       serv = daemon->serverarray[start];
       
     retry:
+      blockdata_retrieve(saved_question, qsize, header);
+      
       *length = htons(qsize);
       
       if (serv->tcpfd == -1)
@@ -2192,9 +2197,11 @@ static ssize_t tcp_talk(int first, int last, int start, unsigned char *packet,  
       serv->flags |= SERV_GOT_TCP;
       
       *servp = serv;
+      blockdata_free(saved_question);
       return rsize;
     }
-
+  
+  blockdata_free(saved_question);
   return 0;
 }
 		  
@@ -2252,7 +2259,7 @@ int tcp_from_udp(time_t now, int status, struct dns_header *header, ssize_t *ple
 
 	  if (n >= daemon->edns_pktsz)
 	    {
-	      /* still too bug, strip optional sections and try again. */
+	      /* still too bIg, strip optional sections and try again. */
 	      new_header->nscount = htons(0);
 	      new_header->arcount = htons(0);
 	      n = resize_packet(new_header, n, NULL, 0);
@@ -2272,8 +2279,8 @@ int tcp_from_udp(time_t now, int status, struct dns_header *header, ssize_t *ple
 	}
     }
   
-   daemon->log_display_id = log_save;
-   free(packet);
+  daemon->log_display_id = log_save;
+  free(packet);
   return new_status;
 }			    
  
@@ -2624,7 +2631,7 @@ unsigned char *tcp_request(int confd, time_t now,
 	       if (saved_question)
 		 blockdata_free(saved_question);
 	       
-	       saved_question = blockdata_alloc((char *) header, (size_t)size);
+	       saved_question = blockdata_alloc((char *)header, (size_t)size);
 	       saved_size = size;
 	       
 	       /* m > 0 if answered from cache */
@@ -2834,8 +2841,7 @@ unsigned char *tcp_request(int confd, time_t now,
       close(confd);
     }
 
-  if (saved_question)
-    blockdata_free(saved_question);
+  blockdata_free(saved_question);
   
   return packet;
 }
