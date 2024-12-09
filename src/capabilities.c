137ac1368ddfd371211fb010d50291349fe1a7cf
DL6ER <dl6er@dl6er.de>
2024-12-09 10:12:56 +0100

    Interchange the arguments for calloc(numer_of_elements, element_size). The current implementation mixed it in a harmless way, however, it is generating a warning in GCC 14 and up. Also, reduce code duplication
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/capabilities.c
+++ src/capabilities.c
@@ -20,12 +20,21 @@
 static const unsigned int capabilityIDs[]   = { CAP_CHOWN ,  CAP_DAC_OVERRIDE ,  CAP_DAC_READ_SEARCH ,  CAP_FOWNER ,  CAP_FSETID ,  CAP_KILL ,  CAP_SETGID ,  CAP_SETUID ,  CAP_SETPCAP ,  CAP_LINUX_IMMUTABLE ,  CAP_NET_BIND_SERVICE ,  CAP_NET_BROADCAST ,  CAP_NET_ADMIN ,  CAP_NET_RAW ,  CAP_IPC_LOCK ,  CAP_IPC_OWNER ,  CAP_SYS_MODULE ,  CAP_SYS_RAWIO ,  CAP_SYS_CHROOT ,  CAP_SYS_PTRACE ,  CAP_SYS_PACCT ,  CAP_SYS_ADMIN ,  CAP_SYS_BOOT ,  CAP_SYS_NICE ,  CAP_SYS_RESOURCE ,  CAP_SYS_TIME ,  CAP_SYS_TTY_CONFIG ,  CAP_MKNOD ,  CAP_LEASE ,  CAP_AUDIT_WRITE ,  CAP_AUDIT_CONTROL ,  CAP_SETFCAP };
 static const char*        capabilityNames[] = {"CAP_CHOWN", "CAP_DAC_OVERRIDE", "CAP_DAC_READ_SEARCH", "CAP_FOWNER", "CAP_FSETID", "CAP_KILL", "CAP_SETGID", "CAP_SETUID", "CAP_SETPCAP", "CAP_LINUX_IMMUTABLE", "CAP_NET_BIND_SERVICE", "CAP_NET_BROADCAST", "CAP_NET_ADMIN", "CAP_NET_RAW", "CAP_IPC_LOCK", "CAP_IPC_OWNER", "CAP_SYS_MODULE", "CAP_SYS_RAWIO", "CAP_SYS_CHROOT", "CAP_SYS_PTRACE", "CAP_SYS_PACCT", "CAP_SYS_ADMIN", "CAP_SYS_BOOT", "CAP_SYS_NICE", "CAP_SYS_RESOURCE", "CAP_SYS_TIME", "CAP_SYS_TTY_CONFIG", "CAP_MKNOD", "CAP_LEASE", "CAP_AUDIT_WRITE", "CAP_AUDIT_CONTROL", "CAP_SETFCAP"};
 
-bool check_capability(const unsigned int cap)
+/**
+ * @brief Retrieves the capabilities of the current process.
+ *
+ * This function determines the capabilities version used by the current kernel
+ * and retrieves the current capabilities of the process.
+ *
+ * @param data Pointer to a cap_user_data_t structure where the capabilities
+ *             will be stored. The memory for this structure is allocated within
+ *             the function and should be freed by the caller.
+ */
+static void get_caps(cap_user_data_t *data)
 {
 	// First assume header version 1
 	int capsize = 1; // VFS_CAP_U32_1
-	cap_user_data_t data = NULL;
-	cap_user_header_t hdr = calloc(sizeof(*hdr), capsize);
+	cap_user_header_t hdr = calloc(capsize, sizeof(*hdr));
 
 	// Determine capabilities version used by the current kernel
 	capget(hdr, NULL);
@@ -48,49 +57,50 @@ bool check_capability(const unsigned int cap)
 	}
 
 	// Get current capabilities
-	data = calloc(sizeof(*data), capsize);
-	capget(hdr, data);
+	*data = calloc(capsize, sizeof(**data));
+	capget(hdr, *data);
+
+	// Free allocated memory
+	free(hdr);
+}
+
+/**
+ * @brief Checks if a specific capability is available.
+ *
+ * This function retrieves the current capabilities of the process and checks if
+ * the specified capability is both permitted and effective.
+ *
+ * @param cap The capability to check.
+ * @return true if the capability is available, false otherwise.
+ */
+bool check_capability(const unsigned int cap)
+{
+	cap_user_data_t data = NULL;
+	get_caps(&data);
 
 	// Check if the capability is available
 	const bool available = ((data->permitted & (1 << cap)) && (data->effective & (1 << cap)));
 
 	// Free memory
-	free(hdr);
 	free(data);
 
 	return available;
 }
 
+/**
+ * @brief Checks the required Linux capabilities for the application.
+ *
+ * This function retrieves the current Linux capabilities and logs the status of
+ * each capability. It then checks if the necessary capabilities for the
+ * application are available and logs warnings if any required capability is
+ * missing.
+ *
+ * @return true if all required capabilities are available, false otherwise.
+ */
 bool check_capabilities(void)
 {
-	// First assume header version 1
-	int capsize = 1; // VFS_CAP_U32_1
 	cap_user_data_t data = NULL;
-	cap_user_header_t hdr = calloc(sizeof(*hdr), capsize);
-
-	// Determine capabilities version used by the current kernel
-	capget(hdr, NULL);
-
-	// Check version
-	if (hdr->version != LINUX_CAPABILITY_VERSION_1)
-	{
-		// If unknown version, use largest supported version (3)
-		// Version 2 is deprecated according to linux/capability.h
-		if (hdr->version != LINUX_CAPABILITY_VERSION_2)
-		{
-			hdr->version = LINUX_CAPABILITY_VERSION_3;
-			capsize = 2; // VFS_CAP_U32_3
-		}
-		else
-		{
-			// Use version 2
-			capsize = 2; // VFS_CAP_U32_2
-		}
-	}
-
-	// Get current capabilities
-	data = calloc(sizeof(*data), capsize);
-	capget(hdr, data);
+	get_caps(&data);
 
 	log_debug(DEBUG_CAPS, "***************************************");
 	log_debug(DEBUG_CAPS, "* Linux capability debugging enabled  *");
@@ -150,7 +160,6 @@ bool check_capabilities(void)
 	}
 
 	// Free allocated memory
-	free(hdr);
 	free(data);
 
 	// Return whether capabilities are all okay
