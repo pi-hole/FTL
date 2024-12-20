45802166331acc884972d1dc5accc55e9e647845
Matthias Andree <matthias.andree@gmx.de>
2024-12-20 13:25:38 +0100

    bpf.c: Follow callback updates.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/bpf.c
+++ src/dnsmasq/bpf.c
@@ -100,14 +100,14 @@ int arp_enumerate(void *parm, callback_t callback)
 #endif /* defined(HAVE_BSD_NETWORK) && !defined(__APPLE__) */
 
 
-int iface_enumerate(int family, void *parm, int (*callback)())
+int iface_enumerate(int family, void *parm, callback_t callback)
 {
   struct ifaddrs *head, *addrs;
   int errsave, fd = -1, ret = 0;
 
   if (family == AF_UNSPEC)
 #if defined(HAVE_BSD_NETWORK) && !defined(__APPLE__)
-    return  arp_enumerate(parm, callback.af_unspec);
+    return  arp_enumerate(parm, callback);
 #else
   return 0; /* need code for Solaris and MacOS*/
 #endif
