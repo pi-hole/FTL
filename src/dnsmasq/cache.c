8eda7e87bae265b48ba17d43c3dfbd82887bd48f
Matthias Andree <matthias.andree@gmx.de>
2024-12-29 22:02:21 +0100

    cache: Fix potential NULL deref in arcane situations.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/dnsmasq/cache.c
+++ src/dnsmasq/cache.c
@@ -496,7 +496,7 @@ static struct crec *cache_scan_free(char *name, union all_addr *addr, unsigned s
 	  if ((crecp->flags & F_FORWARD) && hostname_isequal(cache_get_name(crecp), name))
 	    {
 	      int rrmatch = 0;
-	      if (crecp->flags & flags & F_RR)
+	      if (addr && (crecp->flags & flags & F_RR))
 		{
 		  unsigned short rrc = (crecp->flags & F_KEYTAG) ? crecp->addr.rrblock.rrtype : crecp->addr.rrdata.rrtype;
 		  unsigned short rra = (flags & F_KEYTAG) ? addr->rrblock.rrtype : addr->rrdata.rrtype;
