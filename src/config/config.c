e2620b05993c2489b94e7b444789c1662ca3e1b6
Manakuremati <74898405+Manakuremati@users.noreply.github.com>
2025-05-20 16:23:46 +0200

    move dns.localise to end of main structure dns
    Signed-off-by: Manakuremati <74898405+Manakuremati@users.noreply.github.com>

--- src/config/config.c
+++ src/config/config.c
@@ -579,6 +579,13 @@ static void initConfig(struct config *conf)
 	conf->dns.port.d.ui = 53u;
 	conf->dns.port.c = validate_stub; // Only type-based checking
 
+	conf->dns.localise.k = "misc.dnsmasq_localiseQueries";
+	conf->dns.localise.h = "Enable/Disable the localise-queries option for dnsmasq.\n When this setting is disabled dnsmasq will return all possible values for local DNS Records. Enabled by default";
+	conf->dns.localise.t = CONF_BOOL;
+	conf->dns.localise.f = FLAG_RESTART_FTL;
+	conf->dns.localise.d.b = true;
+	conf->dns.localise.c = validate_stub; // Only type-based checking
+
 	// sub-struct dns.cache
 	conf->dns.cache.size.k = "dns.cache.size";
 	conf->dns.cache.size.h = "Cache size of the DNS server. Note that expiring cache entries naturally make room for new insertions over time. Setting this number too high will have an adverse effect as not only more space is needed, but also lookup speed gets degraded in the 10,000+ range. dnsmasq may issue a warning when you go beyond 10,000+ cache entries.";
@@ -731,13 +738,6 @@ static void initConfig(struct config *conf)
 	conf->dns.reply.blocking.v6.t = CONF_STRUCT_IN6_ADDR;
 	memset(&conf->dns.reply.blocking.v6.d.in6_addr, 0, sizeof(struct in6_addr));
 	conf->dns.reply.blocking.v6.c = validate_stub; // Only type-based checking
-	
-	conf->dns.localise.k = "misc.dnsmasq_localiseQueries";
-	conf->dns.localise.h = "Enable/Disable the localise-queries option for dnsmasq.\n When this setting is disabled dnsmasq will return all possible values for local DNS Records. Enabled by default";
-	conf->dns.localise.t = CONF_BOOL;
-	conf->dns.localise.f = FLAG_RESTART_FTL;
-	conf->dns.localise.d.b = true;
-	conf->dns.localise.c = validate_stub; // Only type-based checking
 
 	// sub-struct dhcp
 	conf->dhcp.active.k = "dhcp.active";
