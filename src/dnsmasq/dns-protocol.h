f3563b80f97f53b52a20f62d4f7549fa51b2715f
darkexplosiveqwx <101737077+darkexplosiveqwx@users.noreply.github.com>
2026-07-31 19:26:16 +0200

    dns: move T_HTTPS definition into numeric order
    Signed-off-by: darkexplosiveqwx <101737077+darkexplosiveqwx@users.noreply.github.com>

--- src/dnsmasq/dns-protocol.h
+++ src/dnsmasq/dns-protocol.h
@@ -97,14 +97,15 @@
 #define T_NSEC          47
 #define T_DNSKEY        48
 #define T_NSEC3         50
+/*************** Pi-hole modification ****************/
+#define T_HTTPS         65
+/*****************************************************/
 #define	T_TKEY		249		
 #define	T_TSIG		250
 #define T_AXFR          252
 #define T_MAILB		253	
 #define T_ANY		255
 #define T_CAA           257
-
-#define T_HTTPS         65
 
 #define EDNS0_OPTION_MAC            65001 /* dyndns.org temporary assignment */
 #define EDNS0_OPTION_CLIENT_SUBNET  8     /* IANA */
