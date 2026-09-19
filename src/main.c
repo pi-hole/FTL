315248841c49e4b12d142883a996e32f55fbf586
DL6ER <dl6er@dl6er.de>
2026-09-19 16:54:01 +0200

    Leave aborting the query import to `terminate_threads()`
    `main()` only has to skip the final export while the import is unfinished. Setting `killed` and interrupting the import is what `terminate_threads()` does right afterwards.
    
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/main.c
+++ src/main.c
@@ -180,16 +180,11 @@ int main (int argc, char *argv[])
 	// be terminating immediately
 	sleepms(250);
 
-	if(!db_import_done)
+	// Save new queries to database. The initial import still occupies the
+	// in-memory database when it is not done, and there is nothing to export
+	// before it is. terminate_threads() aborts it
+	if(db_import_done)
 	{
-		// The initial import still occupies the in-memory database.
-		// Abort it, there is nothing to export before it is done
-		killed = true;
-		interrupt_memdb();
-	}
-	else
-	{
-		// Save new queries to database
 		export_queries_to_disk(true);
 		log_info("Finished final database update");
 	}
